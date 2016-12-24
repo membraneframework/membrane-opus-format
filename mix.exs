75989c0d6f270e13b3a6b2293b69a98a4db515a2
Marcin Lewandowskig <marcin@radiokit.org>
2016-12-24 15:14:59 +0100

    Simplified mix.exs

--- mix.exs
+++ mix.exs
@@ -3,10 +3,8 @@ defmodule Membrane.Caps.Audio.Opus.Mixfile do
 
   def project do
     [app: :membrane_caps_audio_opus,
-     compilers: Mix.compilers,
      version: "0.0.1",
      elixir: "~> 1.3",
-     elixirc_paths: elixirc_paths(Mix.env),
      description: "Membrane Multimedia Framework (Audio.Opus caps)",
      maintainers: ["Marcin Lewandowski"],
      licenses: ["LGPL"],
@@ -17,19 +15,6 @@ defmodule Membrane.Caps.Audio.Opus.Mixfile do
   end
 
 
-  def application do
-    [applications: [
-      :membrane_core
-    ], mod: {Membrane.Caps.Audio.Opus, []}]
-  end
-
-
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib",]
-
-
-  defp deps do
-    [
-    ]
-  end
+  def application, do: []
+  defp deps, do: []
 end
