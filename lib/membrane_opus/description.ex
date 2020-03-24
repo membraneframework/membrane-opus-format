44edcae43a9547d69f6557fb351b10e7e6dd3f41
Marek Kaput <marek.kaput@swmansion.com>
2020-03-24 12:14:43 +0100

    improve docs and change some names

--- lib/membrane_opus/description.ex
+++ lib/membrane_opus/description.ex
@@ -1,37 +1,46 @@
 defmodule Membrane.Opus.Description do
   @moduledoc """
-  This module implements struct describing a single Opus-encoded audio frame.
+  This module implements struct describing an Opus-encoded audio stream.
+
+  Based on [RFC 6716](https://tools.ietf.org/html/rfc6716).
   """
 
-  # Opus frame duration type.
-  #
-  # 2 means 2.5ms, other values just map into duration in miliseconds.
-  @type frame_duration_t :: 2 | 5 | 10 | 20 | 40 | 60
+  # TODO Support combined multiple frames (up to 120ms).
+  @typedoc """
+  Opus frame duration in milliseconds.
+  """
+  @type frame_duration_t :: 2_5 | 5 | 10 | 20 | 40 | 60
 
-  # Amount of channels inside Opus frame.
-  #
-  # Can be 1 or 2.
+  @typedoc """
+  Number of channels transmitted in the stream.
+  """
   @type channels_t :: 1 | 2
 
-  # Enable/disable inband forward error correction
-  #
-  # Can be true (FEC enabled) or false (FEC disabled)
-  @type enable_fec_t :: boolean
+  @typedoc """
+  Enable/disable in-band _Forward Error Correction_ (FEC).
+  """
+  @type fec_enabled_t :: boolean
 
-  # Bitrate that was used to encode frame
-  #
-  # Non negative integer, has to be in range <6144, 522240>
+  @typedoc """
+  Bitrate used to encode the stream in `bit/s`.
+
+  Opus supports all bitrates from `6 kbit/s` to `510 kbit/s`,
+  so this value has to be in range `6144` to `522240`.
+  """
   @type bitrate_t :: non_neg_integer
 
   @type t :: %__MODULE__{
           bitrate: bitrate_t,
           frame_duration: frame_duration_t,
           channels: channels_t,
-          enable_fec: enable_fec_t
+          fec_enabled: fec_enabled_t
         }
 
-  defstruct bitrate: nil,
-            frame_duration: nil,
-            channels: nil,
-            enable_fec: nil
+  @enforce_keys [
+    :bitrate,
+    :frame_duration,
+    :channels,
+    :fec_enabled
+  ]
+  defstruct @enforce_keys
 end
