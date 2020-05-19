34fb7ffcd3052a263102c070f28c210c70fbd473
Mateusz Front <mateusz.front@swmansion.com>
2020-05-19 16:36:37 +0200

    Develop (#2)
    * update naming
    
    * update format description
    
    * update readme
    
    * add circle config
    
    * remove fec_enabled field
    
    * update CI

--- lib/membrane_opus.ex
+++ lib/membrane_opus.ex
@@ -1,4 +1,4 @@
-defmodule Membrane.Opus.Description do
+defmodule Membrane.Opus do
   @moduledoc """
   This module implements struct describing an Opus-encoded audio stream.
 
@@ -8,12 +8,7 @@ defmodule Membrane.Opus.Description do
   @typedoc """
   Number of channels transmitted in the stream.
   """
-  @type channels_t :: 1 | 2
-
-  @typedoc """
-  Enable/disable in-band _Forward Error Correction_ (FEC).
-  """
-  @type fec_enabled_t :: boolean
+  @type channels_t :: 1 | 2 | nil
 
   @typedoc """
   Bitrate used to encode the stream in `bit/s`.
@@ -21,18 +16,16 @@ defmodule Membrane.Opus.Description do
   Opus supports all bitrates from `6 kbit/s` to `510 kbit/s`,
   so this value has to be in range `6144` to `522240`.
   """
-  @type bitrate_t :: non_neg_integer
+  @type bitrate_t :: non_neg_integer | nil
 
   @type t :: %__MODULE__{
           bitrate: bitrate_t,
-          channels: channels_t,
-          fec_enabled: fec_enabled_t
+          channels: channels_t
         }
 
   @enforce_keys [
     :bitrate,
-    :channels,
-    :fec_enabled
+    :channels
   ]
   defstruct @enforce_keys
 end
