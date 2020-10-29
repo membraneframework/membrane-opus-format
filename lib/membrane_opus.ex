6a70390993997c082d39f0e3765757c8e2622ca7
Mateusz Front <mateusz.front@swmansion.com>
2020-10-29 15:56:21 +0100

    add self_delimiting field

--- lib/membrane_opus.ex
+++ lib/membrane_opus.ex
@@ -8,7 +8,7 @@ defmodule Membrane.Opus do
   @typedoc """
   Number of channels transmitted in the stream.
   """
-  @type channels_t :: 1 | 2 | nil
+  @type channels_t :: 1 | 2
 
   @typedoc """
   Bitrate used to encode the stream in `bit/s`.
@@ -16,16 +16,22 @@ defmodule Membrane.Opus do
   Opus supports all bitrates from `6 kbit/s` to `510 kbit/s`,
   so this value has to be in range `6144` to `522240`.
   """
-  @type bitrate_t :: non_neg_integer | nil
+  @type bitrate_t :: non_neg_integer
+
+  @typedoc """
+  Determines if stream uses self-delimiting framing.
+
+  Self-delimiting framing provides information necessary to parse
+  uncontainerized Opus stream.
+  """
+  @type self_delimiting_t :: boolean
 
   @type t :: %__MODULE__{
           bitrate: bitrate_t,
-          channels: channels_t
+          channels: channels_t,
+          self_delimiting?: self_delimiting_t
         }
 
-  @enforce_keys [
-    :bitrate,
-    :channels
-  ]
-  defstruct @enforce_keys
+  @enforce_keys [:bitrate, :channels]
+  defstruct @enforce_keys ++ [self_delimiting?: false]
 end
