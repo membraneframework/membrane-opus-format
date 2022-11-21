b52d55ee0a1be04c0d12ce87832d40b319287b05
Daniel Jodłoś <daniel.jodlos@swmansion.com>
2022-11-21 08:58:13 +0100

    Bugbash (#9)
    * Make everything happy
    
    * Unlock unused deps
    
    * Fix dependencies definitions, update deps and fix LICENSE docs error
    
    * Unlock unused deps

--- mix.exs
+++ mix.exs
@@ -10,6 +10,7 @@ defmodule Membrane.Opus.Format.Mixfile do
       version: @version,
       elixir: "~> 1.10",
       deps: deps(),
+      dialyzer: dialyzer(),
 
       # hex
       description: "Membrane Multimedia Framework (Opus Audio Format Description)",
@@ -31,15 +32,29 @@ defmodule Membrane.Opus.Format.Mixfile do
 
   defp deps do
     [
-      {:ex_doc, "~> 0.21.0", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0", only: :dev, runtime: false}
+      {:credo, ">= 0.0.0", runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:dialyxir, ">= 0.0.0", only: :dev, runtime: false}
     ]
+  end
+
+  defp dialyzer() do
+    opts = [
+      flags: [:error_handling]
+    ]
+
+    if System.get_env("CI") == "true" do
+      # Store PLTs in cacheable directory for CI
+      [plt_local_path: "priv/plts", plt_core_path: "priv/plts"] ++ opts
+    else
+      opts
+    end
   end
 
   defp package do
     [
       maintainers: ["Membrane Team"],
-      licenses: ["Apache 2.0"],
+      licenses: ["Apache-2.0"],
       links: %{
         "GitHub" => @github_url,
         "Membrane Framework Homepage" => "https://membraneframework.org"
@@ -50,7 +65,8 @@ defmodule Membrane.Opus.Format.Mixfile do
   defp docs do
     [
       main: "readme",
-      extras: ["README.md"],
+      extras: ["README.md", "LICENSE"],
+      formatters: ["html"],
       source_ref: "v#{@version}",
       nest_modules_by_prefix: [Membrane.Opus]
     ]
